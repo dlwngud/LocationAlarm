1255339f12744d308190de7b98951f5cd960424e
이주형 <70958782+dlwngud@users.noreply.github.com>
2024-06-17 00:14:36 +0900

    [feat] room db hilt

---
+++ app/src/main/java/com/wngud/locationalarm/di/RoomDBModule.kt
@@ -0,0 +1,26 @@
+package com.wngud.locationalarm.di
+
+import android.content.Context
+import androidx.room.Room
+import com.wngud.locationalarm.data.AlarmDao
+import com.wngud.locationalarm.data.AlarmDatabase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object RoomDBModule {
+
+    @Provides
+    @Singleton
+    fun provideAlarmDatabase(@ApplicationContext context: Context): AlarmDatabase =
+        Room.databaseBuilder(context, AlarmDatabase::class.java, "alarm_table").build()
+
+    @Provides
+    @Singleton
+    fun provideAlarmDao(alarmDatabase: AlarmDatabase): AlarmDao = alarmDatabase.alarmDao()
+}
