8faebfdc1666cfcec7e66b67f791ea37ad23cb8b
이주형 <70958782+dlwngud@users.noreply.github.com>
2024-06-22 23:59:23 +0900

    [feat] 지도 현재 위치로 바로 카메라 이동

--- app/src/main/java/com/wngud/locationalarm/screen/home/HomeScreen.kt
+++ app/src/main/java/com/wngud/locationalarm/screen/home/HomeScreen.kt
@@ -1,16 +1,64 @@
 package com.wngud.locationalarm.screen.home
 
+import android.util.Log
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.naver.maps.geometry.LatLng
+import com.naver.maps.map.CameraPosition
 import com.naver.maps.map.compose.ExperimentalNaverMapApi
+import com.naver.maps.map.compose.LocationTrackingMode
+import com.naver.maps.map.compose.MapProperties
+import com.naver.maps.map.compose.MapUiSettings
 import com.naver.maps.map.compose.NaverMap
+import com.naver.maps.map.compose.rememberCameraPositionState
+import com.naver.maps.map.compose.rememberFusedLocationSource
+
 
 @OptIn(ExperimentalNaverMapApi::class)
 @Composable
 fun HomeScreen() {
-    NaverMap(modifier = Modifier.fillMaxSize())
+    var mapProperties by remember {
+        mutableStateOf(
+            MapProperties(
+                maxZoom = 18.0, minZoom = 10.0, locationTrackingMode = LocationTrackingMode.Follow
+            )
+        )
+    }
+    var mapUiSettings by remember {
+        mutableStateOf(
+            MapUiSettings(isLocationButtonEnabled = true)
+        )
+    }
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        val locationSource = rememberFusedLocationSource()
+        val cameraPositionState = rememberCameraPositionState()
+
+        LaunchedEffect(locationSource) {
+            locationSource.activate { location ->
+                Log.i("location", location.toString())
+                location?.let {
+                    cameraPositionState.position = CameraPosition(LatLng(location.latitude, location.longitude), 10.0)
+                }
+            }
+        }
+
+        NaverMap(
+            properties = mapProperties,
+            uiSettings = mapUiSettings,
+            locationSource = locationSource,
+            cameraPositionState = cameraPositionState
+        )
+    }
 }
 
 @Preview(showBackground = true)
