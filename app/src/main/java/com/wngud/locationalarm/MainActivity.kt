144d2d8dfef0e3185ee6014aa32b697feaed48f1
이주형 <70958782+dlwngud@users.noreply.github.com>
2024-06-25 18:40:05 +0900

    [style] 불필요한 코드 삭제

--- app/src/main/java/com/wngud/locationalarm/MainActivity.kt
+++ app/src/main/java/com/wngud/locationalarm/MainActivity.kt
@@ -5,13 +5,8 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.compose.rememberNavController
 import com.wngud.locationalarm.ui.theme.LocationAlarmTheme
 
 class MainActivity : ComponentActivity() {
