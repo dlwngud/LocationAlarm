0a776ac0e2be2a8aef52f10d2b7a58158ad7bb45
이주형 <70958782+dlwngud@users.noreply.github.com>
2024-06-25 23:07:46 +0900

    [feat] 지도 롱클릭시 마커와 범위 생성

--- app/src/main/java/com/wngud/locationalarm/screen/home/HomeScreen.kt
+++ app/src/main/java/com/wngud/locationalarm/screen/home/HomeScreen.kt
@@ -3,6 +3,7 @@ package com.wngud.locationalarm.screen.home
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -10,12 +11,16 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.CameraPosition
+import com.naver.maps.map.compose.CircleOverlay
 import com.naver.maps.map.compose.ExperimentalNaverMapApi
 import com.naver.maps.map.compose.LocationTrackingMode
 import com.naver.maps.map.compose.MapProperties
 import com.naver.maps.map.compose.MapUiSettings
+import com.naver.maps.map.compose.Marker
+import com.naver.maps.map.compose.MarkerState
 import com.naver.maps.map.compose.NaverMap
 import com.naver.maps.map.compose.rememberCameraPositionState
 import com.naver.maps.map.compose.rememberFusedLocationSource
@@ -47,6 +52,7 @@ fun HomeScreen(onBackPressed: () -> Unit) {
         val locationSource = rememberFusedLocationSource()
         val cameraPositionState = rememberCameraPositionState()
         var isFirstLoad by remember { mutableStateOf(true) }
+        var isMapClick by remember { mutableStateOf(LatLng(-1.0, -1.0)) }
 
         NaverMap(
             properties = mapProperties,
@@ -59,9 +65,31 @@ fun HomeScreen(onBackPressed: () -> Unit) {
                         CameraPosition(LatLng(it.latitude, it.longitude), 15.0)
                     isFirstLoad = false
                 }
+            },
+            onMapLongClick = { _, latLng ->
+                isMapClick = latLng
             }
-        )
+        ) {
+            if (isMapClick != LatLng(-1.0, -1.0)) {
+                showMarker(latLng = isMapClick)
+            }
+        }
     }
+}
+
+@OptIn(ExperimentalNaverMapApi::class)
+@Composable
+fun showMarker(latLng: LatLng) {
+    Marker(
+        state = MarkerState(position = latLng),
+    )
+    CircleOverlay(
+        center = latLng,
+        radius = 100.0,
+        outlineColor = MaterialTheme.colorScheme.primary,
+        outlineWidth = 2.dp,
+        color = MaterialTheme.colorScheme.primaryContainer.copy(alpha = 0.5f)
+    )
 }
 
 @Preview(showBackground = true)
