f8260c196f51ad338de3e070e7d2a45b125a28a8
이주형 <leewngud980411@gmail.com>
2024-12-09 16:59:12 +0900

    [feat] 알림 채널 생성

--- app/src/main/java/com/wngud/locationalarm/App.kt
+++ app/src/main/java/com/wngud/locationalarm/App.kt
@@ -1,7 +1,13 @@
 package com.wngud.locationalarm
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import com.naver.maps.map.NaverMapSdk
+import com.wngud.locationalarm.Constants.GEOFENCE_CHANNEL_ID
+import com.wngud.locationalarm.Constants.SERVICE_CHANNEL_ID
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
@@ -10,5 +16,30 @@ class App: Application() {
     override fun onCreate() {
         super.onCreate()
         NaverMapSdk.getInstance(this).client = NaverMapSdk.NaverCloudPlatformClient(BuildConfig.NAVER_CLIENT_ID)
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+            val serviceChannel = NotificationChannel(
+                SERVICE_CHANNEL_ID,
+                "Service",
+                NotificationManager.IMPORTANCE_HIGH
+            ).apply {
+                setShowBadge(true)
+            }
+            manager.createNotificationChannel(serviceChannel)
+
+            val geofenceChannel = NotificationChannel(
+                GEOFENCE_CHANNEL_ID,
+                "Geofence Monitoring",
+                NotificationManager.IMPORTANCE_HIGH
+            ).apply {
+                setShowBadge(true)
+            }
+            manager.createNotificationChannel(geofenceChannel)
+        }
     }
 }
---
+++ app/src/main/java/com/wngud/locationalarm/Constants.kt
@@ -0,0 +1,6 @@
+package com.wngud.locationalarm
+
+object Constants {
+    const val SERVICE_CHANNEL_ID = "SERVICE_CHANNEL"
+    const val GEOFENCE_CHANNEL_ID = "GEOFENCE_CHANNEL"
+}
