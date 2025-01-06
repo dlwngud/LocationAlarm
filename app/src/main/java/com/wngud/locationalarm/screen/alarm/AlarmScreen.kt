49951eee1f37caf3036dda924989eb5b8663b7cd
이주형 <leewngud980411@gmail.com>
2025-01-06 20:00:36 +0900

    [feat] LazyColumn bottom padding 추가

--- app/src/main/java/com/wngud/locationalarm/screen/alarm/AlarmScreen.kt
+++ app/src/main/java/com/wngud/locationalarm/screen/alarm/AlarmScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -110,7 +111,8 @@ fun AlarmScreen(
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(it)
+                .padding(it),
+            contentPadding = PaddingValues(bottom = 8.dp)
         ) {
             items(alarmState.alarms, key = { wish -> wish.id }) { alarm ->
                 StyledCard(alarm, navController, alarmViewModel = alarmViewModel, context = context)
